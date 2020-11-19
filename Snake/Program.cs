0ef11dd2d18194dc5a2238d440988bec2ef96953
Ced <cbourneville@gmail.com>
2020-11-19 14:23:44 +1300

    Initial commit
    Snake moves, but doesn't recieve input, and doesn't die.
    Console window stays open awaiting cancellation, which is not yet implemented

---
+++ Snake/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+//using System.Timers;
+
+namespace Snake
+{
+    class Program
+    {
+        static ManualResetEvent _manualResetEvent = new ManualResetEvent(false);
+
+        const int MapWidth = 20;
+        const int MapHeight = 10;
+        static Snake snake;
+        static Timer timer;
+
+        public static void Main(string[] args)
+        {
+            snake = new Snake(MapWidth, MapHeight);
+            timer = new Timer(Tick, _manualResetEvent, 1000, 1000);
+
+            _manualResetEvent.WaitOne();
+        }
+
+        private static void Tick(object state)
+        {
+            snake.Move();
+            Draw();
+        }
+
+        public static void Draw()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            for (int y = 0; y < MapHeight; y++)
+            {
+                for(int x = 0; x < MapWidth; x++)
+                {
+                    sb.Append(snake.OccupiesSquare(x, y) ? "#" : " ");
+                }
+                sb.AppendLine();
+            }
+            sb.AppendLine($"\nSnake Position: Width: {snake.Head.X}, Height: {snake.Head.Y}, Length: {snake.Length}");
+
+            Console.Clear();
+            Console.Write(sb.ToString());
+        }
+    }
+}
---
+++ Snake/Snake.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Snake
+{
+    public class Snake
+    {
+        private List<(int X, int Y)> _snake;
+        private Direction _direction;
+        private readonly int _mapWidth, _mapHeight;
+
+        public Snake(int mapWidth, int mapHeight)
+        {
+            _mapWidth = mapWidth;
+            _mapHeight = mapHeight;
+
+            _snake = new List<(int X, int Y)>
+            {
+                (mapWidth / 2, mapHeight / 2)
+            };
+            _direction = Direction.East;
+            Length = 3;
+        }
+
+        public void Move()
+        {
+            var currentHeadX = _snake.First().X;
+            var currentHeadY = _snake.First().Y;
+
+            var newHead = _direction switch
+            {
+                Direction.North => (currentHeadX, currentHeadY - 1),
+                Direction.South => (currentHeadX, currentHeadY + 1),
+                Direction.East => (currentHeadX + 1, currentHeadY),
+                Direction.West => (currentHeadX - 1, currentHeadY),
+                _ => throw new NotImplementedException()
+            };
+
+            _snake.Insert(0, newHead);
+            _snake = _snake.Take(Length).ToList();
+        }
+
+        public bool OccupiesSquare(int x, int y)
+        {
+            return _snake.Contains((x, y));
+        }
+
+        public int Length { get; private set; }
+
+        public (int X, int Y) Head => _snake.First();
+
+    }
+
+    public enum Direction
+    {
+        North,
+        South,
+        East,
+        West
+    }
+}
